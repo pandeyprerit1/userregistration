29525fb5012d459d15f9af5119a88eae69b88ba4
Pandey <prerit.pandey@accenture.com>
2021-02-28 17:45:11 +0530

    login-service

--- src/App.js
+++ src/App.js
@@ -1,25 +1,237 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Amplify, {Auth, API} from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifySignOut,AmplifySignUp,AmplifySignIn, AmplifyConfirmSignUp, AmplifyForgotPassword } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsconfig from './aws-exports';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsconfig);
+
+const AuthStateApp = () => {
+    const [authState, setAuthState] = React.useState();
+    const [user, setUser] = React.useState();
+    const [username, setUsername] = React.useState();
+    const [email, setEmail] = React.useState();
+    const [password, setPassword] = React.useState(); 
+    const [authCode, setAuthCode] = React.useState(); 
+    const [firstname, setFirstname] = React.useState(); 
+    const [lastname, setLastname] = React.useState(); 
+    const [phonenumber, setPhonenumber] = React.useState(); 
+    const [billingaddress, setBillingaddress] = React.useState(); 
+    const [shippingAddress, setShippingAddress] = React.useState();
+    const [clientId, setClientId] = React.useState(); 
+    const [errorMessage, setErrorMessage] = React.useState();
+       
+    React.useEffect(() => {
+        return onAuthUIStateChange((nextAuthState, authData) => {
+            setAuthState(nextAuthState);
+            setUser(authData);
+        });
+    }, []);
+
+    const signUp = (event) => {
+      if(email==''){
+        setErrorMessage("Email Cant Be Null");
+        setAuthState(AuthState.SignUp);
+      }
+      else{
+       try {
+       
+      const { user } =  Auth.signUp({username, password, attributes: {email}}).then( data => {
+        setClientId(data.user.pool.clientId)
+        console.log("data",data)
+        setUser(user)   
+         setAuthState(AuthState.ConfirmSignUp)
+        return Auth.currentUserPoolUser( );
+    } ).catch(err => setErrorMessage(err.message));
+      }catch(error){
+        console.log('error signing up:', error);
+      } 
+      }
+    }
+
+    const confirmSignUp = (event) => {
+      console.log('code :-- ',authCode)
+      Auth.confirmSignUp(username, authCode).then( data => {
+        console.log("currentUserPoolUser",Auth.currentUserPoolUser( ))
+        console.log("currentUserInfo",Auth.currentUserInfo)
+        console.log("currentUserCredentials",Auth.currentUserCredentials)
+        console.log("data",data)
+        return Auth.currentUserPoolUser( );
+    } )
+    .then( user => {
+        console.log(user);
+        setUser(user)
+      console.log("User",user)
+      setAuthState(AuthState.SignIn);
+        API.post('userservices','/createusers',{
+        body:{
+          id:clientId,
+  firstname:firstname,
+  lastname:lastname,
+  username:username,
+  contactno:phonenumber
+        }
+      })  
+    } ).catch(err => this.setError({errMsg: err.message})) ;
+      
+    }
+
+    const signIn = (event) => {
+      event.preventDefault();
+       try {
+      const { user1 } =  Auth.signIn({username, password});
+      setUser(user1)   
+         setAuthState(AuthState.SignedIn)
+      }catch(error){
+        console.log('error signing up:', error);
+      } 
+    }
+    
+    if(authState === AuthState.SignedIn){
+      return (
+        <div className="App">
+            <div>Hello You Loged in !</div>
+             <AmplifySignOut/>
+           </div>
+      ) 
+    }else if(authState === AuthState.ConfirmSignUp ){
+      return (
+        <div className="App">
+          <AmplifyConfirmSignUp user={user} slot="confirm-sign-up" headerText={"Enter verification code"} usernameAlias="username" handleSubmit={confirmSignUp}
+          formFields={[
+            {
+              type: "username",
+              label: "username",
+              placeholder: "username",
+              required: true,
+              handleInputChange: (event, cb) => {
+                cb(event);
+                setUsername(event.target.value)
+              },
+            },
+            {
+              type: "code",
+              label: "authCode",
+              placeholder: "authCode",
+              required: true,
+              handleInputChange: (event, cb) => {
+                cb(event);
+                setAuthCode(event.target.value)
+              },
+            },          
+          ]}
+          /> 
+          </div>)
+    }
+    else if(authState === AuthState.SignIn){
+      return (
+        <div className="App">
+         <AmplifyAuthenticator>
+           <AmplifySignIn/>
+           </AmplifyAuthenticator>
+            </div>)
+    }
+    else if(authState === AuthState.ForgotPassword){
+      return (
+        <div className="App">
+         <AmplifyForgotPassword></AmplifyForgotPassword>
+            </div>)
+    }
+    else{
+      return (
+        <div className="App">
+          <p>{errorMessage ? <p style={{ color: "red" }}>{errorMessage}</p> : null}</p>
+          <AmplifySignUp slot="sign-up" handleSubmit={signUp}
+          formFields={[
+              {
+                type: "username",
+                label: "username",
+                placeholder: "Please Enter Username",
+                required: true,
+                handleInputChange: (event, cb) => {
+                  cb(event);
+                  setUsername(event.target.value)
+                },
+              },
+              {
+                type: "email",
+                label: "email",
+                placeholder: "Please Enter Email",
+                required: true,
+                handleInputChange: (event, cb) => {
+                  cb(event);
+                  if(event.target.value===null){
+                    setErrorMessage("Plesae Enter Email");
+                  }else{
+                    setEmail(event.target.value)
+                  }
+                },
+              },
+              {
+                type: "password",
+                label: "password",
+                placeholder: "Please Enter Password",
+                required: true,
+                handleInputChange: (event, cb) => {
+                  cb(event);
+                  setPassword(event.target.value)
+                },
+              },
+              {
+                type: "phone_number",
+                label: "Phone",
+                placeholder: "Please Enter Phone no",
+                required: false,
+                handleInputChange: (event, cb) => {
+                  cb(event);
+                  setPhonenumber(event.target.value)
+                },
+              },
+              {
+                type: "Firstname",
+                label: "Firstname",
+                placeholder: "Please Enter Firstname",
+                required: false,
+                handleInputChange: (event, cb) => {
+                  cb(event);
+                  setFirstname(event.target.value)
+                },
+              },
+              {
+                type: "Lastname",
+                label: "Lastname",
+                placeholder: "Please Enter Lastname",
+                required: false,
+                handleInputChange: (event, cb) => {
+                  cb(event);
+                  setLastname(event.target.value)
+                },
+              },
+              {
+                type: "BillingAddress",
+                label: "BillingAddress",
+                placeholder: "Please Enter Billing Address",
+                required: false,
+                handleInputChange: (event, cb) => {
+                  cb(event);
+                  setBillingaddress(event.target.value)
+                },
+              },
+              {
+                type: "ShippigAddress",
+                label: "ShippigAddress",
+                placeholder: "Please Enter Shipping Address",
+                required: false,
+                handleInputChange: (event, cb) => {
+                  cb(event);
+                  setShippingAddress(event.target.value)
+                },
+              },
+            ]} 
+          />
+          </div>
+      );
+    }
 }
-
-export default App;
+export default AuthStateApp;
